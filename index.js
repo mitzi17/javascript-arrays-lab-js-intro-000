39f253b558aebd415e54edbd36284bce24583ae1
Mitzi Estrada <m_estrada17@live.com>
2020-01-09 06:01:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 var kittens = [ "Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten (name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.shift(name)
+}
+
+function destructivelyRemoveLastKitten (name) {
+  kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten (name) {
+  kittens.shift(name)
+}
+
+function appendKitten (name) {
+  kittens.slice(1)
+
+  return kittens
+}
+
+function prependKitten (name) {
+  return kittens.slice(0, kittens.length - 1)
+}
